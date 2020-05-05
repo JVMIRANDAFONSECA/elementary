ceea4d00b46faf6e731d1f974fd94760af777e6c
Fonseca <jvmirandafonseca@gmail.com>
2020-05-05 21:39:16 +0100

    Created FilterBar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar.js';
 import Table from './components/Table.js';
+import FilterBar from './components/FilterBar.js';
 class App extends Component {
+    
     render() {
         return (
             <div>
                 <Navbar />
+                <FilterBar />
                 <Table />
             </div>
             );
---
+++ src/components/FilterBar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class FilterBar extends Component {
+    
+    render() {
+        return (
+            <p>This will be the filter options and legend bar</p>
+        )
+    }
+}
+
+export default FilterBar
